dad402be5bf8a6a7998f31259df2baeef2912e7b
Alison Moura <alison.moura@hightechcursos.com.br>
2021-09-25 12:21:58 -0400

    Criando rota com fake login

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
+const cors = require("cors")
 const express = require("express")
 const app = express()
+
+// Configuração do express
+app.use(express.json()) // Aceitar req JSON
+app.use(cors({ // CORS
+    origin: "*"
+}))
 
 app.get("/", (req, res) => {
     res.json({
@@ -19,6 +26,18 @@ app.get("/clientes", (req, res) => {
             nome: "Zé da Silva"
         }
     ])
+})
+
+app.post("/login", (req, res) => {
+    if(req.body.email == "jao@gmail.com" && req.body.password == "123") {
+        res.json({
+            _id: "123456",
+            nome: "Jão da Silva Carvalho",
+            email: "jao@gmail.com"
+        })
+    } else {
+        res.status(404).json({ message: "Usuário não encontrado, email ou senha incorretos" })
+    }
 })
 
 app.listen(3000, ()=> {
