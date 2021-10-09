eeb081a26daa58e2274812267c23174c1a6e5a2b
Alison Moura <alison.moura@hightechcursos.com.br>
2021-10-09 15:04:15 -0400

    Fazendo rota cadastro de usuário

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-const cors = require("cors")
+const cors = require("cors") // CommonsJS
 const express = require("express")
 const app = express()
 
 // Configuração do express
 app.use(express.json()) // Aceitar req JSON
-app.use(cors({ // CORS
+app.use(cors({ // CORS (Cross Origin Resource Sharing)
     origin: "*"
 }))
 
@@ -12,6 +12,17 @@ app.get("/", (req, res) => {
     res.json({
         mensagem: "API Feirinha Virtual"
     })
+})
+
+app.post("/users", (req, res) => {
+    console.log(req.body)
+    if(req.body.age < 18) {
+        res.status(400).json({ message: "A idade precisa ser maior ou igual a 18" })
+    } else {
+        res.json({ message: "Usuário salvo com sucesso" })
+    }
+    // TODO: Fazer validações
+    // TODO: Cadastrar req.body no DB
 })
 
 app.get("/clientes", (req, res) => {
@@ -40,6 +51,6 @@ app.post("/login", (req, res) => {
     }
 })
 
-app.listen(3000, ()=> {
+app.listen(3000, ()=> { // "Ligando" a API
     console.log("API rodando na porta 3000")
 })
